3dda75325ed88ebc52745076a234ef0090c159ea
karimax2 <lazaarworks@gmail.com>
2020-10-25 09:52:14 +0100

    initial

---
+++ android/app/src/main/kotlin/com/example/timywebapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.timywebapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:timywebapp/home2.dart';
+import 'package:timywebapp/verticalTab.dart';
+//import 'package:timywebapp/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home3(),
+  ));
+}
