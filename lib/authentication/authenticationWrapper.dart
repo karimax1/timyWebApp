f94444ef090c591e89c89fe267c79a447bf6ee1e
karimax2 <lazaarworks@gmail.com>
2020-11-25 14:30:11 +0100

    NavRail dashboard home

--- lib/authentication/authenticationWrapper.dart
+++ lib/authentication/authenticationWrapper.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:timywebapp/navRail.dart';
+import 'package:timywebapp/widgets/navRail.dart';
 import 'package:timywebapp/pages/welcomePage.dart';
 
 class AuthenticationWrapper extends StatelessWidget {
