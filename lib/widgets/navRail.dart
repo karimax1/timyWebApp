2705a9c009c0bd936979a7775ab534938462d216
karimax2 <lazaarworks@gmail.com>
2020-12-10 17:56:17 +0100

    current user data working first version

--- lib/widgets/navRail.dart
+++ lib/widgets/navRail.dart
@@ -1,15 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:timywebapp/authentication/auth_service.dart';
 import 'package:timywebapp/models/userChannelLink.dart';
 import 'package:timywebapp/pages/main_dashboard/channelInfo.dart';
 import 'package:timywebapp/pages/main_dashboard/dashboard.dart';
 import 'package:timywebapp/pages/main_dashboard/schedule.dart';
 import 'package:timywebapp/pages/main_dashboard/shows.dart';
-import 'package:timywebapp/authentication/auth_service.dart';
-import 'package:provider/provider.dart';
 
-final channelRef = FirebaseFirestore.instance.collection('users');
+final channelRef = FirebaseFirestore.instance.collection("users");
 
 class NavRail extends StatefulWidget {
   final String profileId;
@@ -21,47 +21,48 @@ class NavRail extends StatefulWidget {
 
 class _NavRailState extends State<NavRail> {
   int _selectedIndex = 0;
-  final position = [
-    Dashboard(),
-    ChannelInfo(),
-    Shows(),
-    Schedule(),
-  ];
+  final User user = FirebaseAuth.instance.currentUser;
+  String userid;
+
+  final position = [Dashboard(), ChannelInfo(), Shows(), Schedule()];
 
   @override
   void initState() {
     //getChannels();
+    userid = user.uid;
+    getData(userid);
+
+    print(user.uid + " meeeee");
 
     super.initState();
   }
 
-  // getChannels() async {
-  //   // final String id = "0Hb6Wt5mMdY8AZZDzfkgMYMj4fv2";
-  //   // final DocumentSnapshot doc = await channelRef.doc(id).get();
-  //   final QuerySnapshot snapshot =
-  //       await channelRef.where('genre', isEqualTo: 'Movie').get();
-  //   snapshot.docs.forEach((DocumentSnapshot doc) {
-  //     print(doc.data());
-  //   });
-  // }
+  DocumentSnapshot doc;
+
+  getData(String userId) {
+    channelRef.doc(userId).get().then((value) {
+      setState(() {
+        doc = value;
+      });
+      print(doc.data()["channelName"]);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final _channelData = Provider.of<Stream<List<UserChannelLink>>>(context);
-    // return FutureBuilder(
-    //     future: channelRef.doc().get(),
-    //     builder: (context, AsyncSnapshot snapshot) {
-    //       if (!snapshot.hasData) {
-    //         return circularProgress();
-    //       }
-    //       UserChannelLink _channel =
-    //           UserChannelLink.fromDocument(snapshot.data);
-    //       print(_channel.channelName);
     return StreamProvider(
         create: (_) => _channelData,
         child: Builder(
           builder: (context) {
-            final channelData = Provider.of<List<UserChannelLink>>(context);
+            //final channelData = Provider.of<List<UserChannelLink>>(context);
+
+            if (doc == null) {
+              return Scaffold(
+                body: Container(child: Center(child: Text("Loading..."))),
+              );
+            }
+
             return Scaffold(
               appBar: AppBar(
                 title: Center(child: Text('Timytime')),
@@ -72,12 +73,12 @@ class _NavRailState extends State<NavRail> {
                     child: Container(
                       height: 60,
                       width: 60,
-                      child: Image.network(channelData.first.logoLink),
+                      child: Image.network(doc.data()["logoLink"]),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(10.0),
-                    child: Text(channelData.first.channelName),
+                    child: Text(doc.data()["broadcastRegion"]),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
@@ -147,13 +148,3 @@ class _NavRailState extends State<NavRail> {
         ));
   }
 }
-
-// Container circularProgress() {
-//   return Container(
-//     alignment: Alignment.center,
-//     padding: EdgeInsets.only(top: 10.0),
-//     child: CircularProgressIndicator(
-//       valueColor: AlwaysStoppedAnimation(Colors.purple),
-//     ),
-//   );
-// }
